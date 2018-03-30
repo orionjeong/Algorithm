67373aa0a0de7844cbc49cd45f961a6fceb5af80
orionjeong <djflqjfl0209@gmail.com>
2018-03-30 15:36:10 +0900

    Dynamic Programing 이친수 BottomUp

--- DynamicPrograming/src/이친수.java
+++ DynamicPrograming/src/이친수.java
@@ -3,11 +3,26 @@ import java.util.Scanner;
 
 public class 이친수 {
     static BigInteger memo[];
+    static BigInteger memo2[];
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
         int n=scan.nextInt();
         memo=new BigInteger[n+1];
+        memo2=new BigInteger[91];
         System.out.println(topDown(n));
+        System.out.println(bottomUp(n));
+    }
+
+    public static BigInteger bottomUp(int n){
+        memo2[0]=BigInteger.valueOf(0);
+        memo2[1] = BigInteger.valueOf(1);
+        memo2[2] = BigInteger.valueOf(1);
+
+        for(int i=3; i<=n; i++){
+            memo2[i]=memo2[i-1].add(memo2[i-2]);
+        }
+
+        return memo2[n];
     }
     public static BigInteger topDown(int n){
         if(n==0){
