c3fef3716ed16c5a884acf4b9d36c1ba1231c7e2
orionjeong <djflqjfl0209@gmail.com>
2018-04-03 10:06:27 +0900

    Dynamic Programming 쉬운계단수 Algorithm

--- DynamicPrograming/src/쉬운계단수.java
+++ DynamicPrograming/src/쉬운계단수.java
@@ -2,24 +2,33 @@ import java.util.Scanner;
 
 public class 쉬운계단수 {
     static int[] memo;
-
-
+    static private int MOD=1000000000;
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        memo = new int[101];
-        memo[0]=0;
-        memo[1]=9;
-        memo[2]=17;
-        System.out.println(topDown(n)%1000000000);
-    }
-
-    public static int topDown(int n){
-
-        if(memo[n]>0){
-            return memo[n];
+        int [][] memo = new int[n+1][10];
+        int add=0;
+        for(int i=1; i<10; i++){
+            memo[1][i]=1;
         }
-        memo[n]= (8*topDown(n-1)+ 2*topDown(n-2))%1000000000;
-        return memo[n];
+        for(int j=2; j<=n; j++){
+            for(int z=0; z<=9; z++){
+                if(z==0){
+                    memo[j][0]= memo[j-1][1]%MOD;
+                }
+                if(z>=1 && z<=8){
+                    memo[j][z]=0;
+                    memo[j][z]= (memo[j-1][z-1]+memo[j-1][z+1])%MOD;
+                }
+                if(z==9){
+                    memo[j][9]= memo[j-1][8]%MOD;
+                }
+            }
+        }
+        for(int i=0; i<memo[n].length; i++){
+            add =(add+memo[n][i])%MOD;
+        }
+        System.out.println(add);
     }
+
 }
