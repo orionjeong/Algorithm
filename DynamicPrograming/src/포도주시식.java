f358727cc760fedfd9c4f9216df0a58b4e76fedf
orionjeong <djflqjfl0209@gmail.com>
2018-05-12 22:49:22 +0900

    포도주시식1,2

--- DynamicPrograming/src/포도주시식.java
+++ DynamicPrograming/src/포도주시식.java
@@ -4,21 +4,29 @@ import java.io.InputStreamReader;
 
 public class 포도주시식 {
     public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n =Integer.parseInt(br.readLine());
-        int amount[] =new int[n];
-        int memo[][] = new int[n+1][3];
-        for(int i=0; i<n; i++){
-            amount[i]=Integer.parseInt(br.readLine());
-        }
-
-        for(int i=1; i<=n; i++){
-            memo[i][0]=Math.max(memo[i-1][0], Math.max(memo[i-1][1], memo[i-1][2]));
-            memo[i][1]=memo[i-1][0]+amount[i-1];
-            memo[i][2]=memo[i-1][1]+amount[i-1];
-        }
-
-
-        System.out.println(Math.max(memo[n][0], Math.max(memo[n][1],memo[n][2])));
+     BufferedReader buffferReader = new BufferedReader(new InputStreamReader(System.in));
+     int cupNumber = Integer.parseInt(buffferReader.readLine());
+     int[] cupVolume = new int[cupNumber+1];
+     int[] memo = new int[cupNumber+1];
+     for(int i = 1; i<cupNumber+1; i++){
+         cupVolume[i]= Integer.parseInt(buffferReader.readLine());
+     }
+        memo[1] = cupVolume[1];
+     if(cupNumber>1){
+         memo[2] = cupVolume[1]+cupVolume[2];
+     }
+     for(int i=3; i<cupNumber+1; i++){
+        memo[i] = Math.max(memo[i-1], Math.max(cupVolume[i]+memo[i-2],cupVolume[i]+cupVolume[i-1]+ memo[i-3]));
+//         memo[i]= memo[i-1];
+//         if(memo[i]< cupVolume[i]+memo[i-2]){
+//             memo[i]= cupVolume[i]+memo[i-2];
+//         }
+//         if(memo[i]< cupVolume[i]+cupVolume[i-1]+ memo[i-3]){
+//             memo[i] = cupVolume[i]+cupVolume[i-1]+ memo[i-3];
+//         }
+     }
+        System.out.println(memo[cupNumber]);
     }
+
+
 }
---
+++ DynamicPrograming/src/포도주시식2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 포도주시식2 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n =Integer.parseInt(br.readLine());
+        int amount[] =new int[n];
+        int memo[][] = new int[n+1][3];
+        for(int i=0; i<n; i++){
+            amount[i]=Integer.parseInt(br.readLine());
+        }
+
+        for(int i=1; i<=n; i++){
+            memo[i][0]=Math.max(memo[i-1][0], Math.max(memo[i-1][1], memo[i-1][2]));
+            memo[i][1]=memo[i-1][0]+amount[i-1];
+            memo[i][2]=memo[i-1][1]+amount[i-1];
+        }
+
+
+        System.out.println(Math.max(memo[n][0], Math.max(memo[n][1],memo[n][2])));
+    }
+}
